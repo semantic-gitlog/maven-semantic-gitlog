f478ce3509f4cddbfb3494e6c4ca2175d1aadcea
ymind <ymind@yi.team>
2020-02-06 15:27:47 +0800

    feat(mojo): sorted `ReleaseLog.tags`

--- src/main/java/team/yi/maven/plugin/model/ReleaseLog.java
+++ src/main/java/team/yi/maven/plugin/model/ReleaseLog.java
@@ -4,7 +4,9 @@ import de.skuzzle.semantic.Version;
 import lombok.Value;
 
 import java.io.Serializable;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Value
 public class ReleaseLog implements Serializable {
@@ -15,4 +17,28 @@ public class ReleaseLog implements Serializable {
     private Version nextVersion;
     private Version lastVersion;
     private List<ReleaseTag> tags;
+
+    public List<ReleaseTag> getTags() {
+        if (tags == null || tags.isEmpty()) return null;
+
+        return tags.stream()
+            .distinct()
+            .sorted(Comparator.nullsFirst((Comparator<ReleaseTag>) (o1, o2) -> {
+                if (o2 == null) return -1;
+
+                final ReleaseDate t1 = o1.getReleaseDate();
+                final ReleaseDate t2 = o2.getReleaseDate();
+
+                if (t1 == null && t2 == null) {
+                    return 0;
+                } else if (t1 == null) {
+                    return 1;
+                } else if (t2 == null) {
+                    return -1;
+                }
+
+                return t1.getDate().compareTo(t2.getDate());
+            }).reversed())
+            .collect(Collectors.toList());
+    }
 }
